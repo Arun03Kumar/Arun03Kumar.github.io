6ca05ae36313d363c956f2426848836b46b86a06
Arun03Kumar <0arunkumar14@gmail.com>
2023-09-03 03:42:36 +0530

    added popup

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
-let btns = document.querySelectorAll('.control')
-let sectbtn = document.querySelectorAll('.controls')
+let btns = document.querySelectorAll(".control");
+let sectbtn = document.querySelectorAll(".controls");
+let dialouge = document.querySelector(".dialouge");
 // console.log(sectbtn)
 // console.log(btns)
-function pageChange()
-{
-  for(let i = 0; i < btns.length; i++)
-  {
+const closeDialouge = () => {
+  dialouge.style.display = "none";
+};
+function pageChange() {
+  for (let i = 0; i < btns.length; i++) {
     // const sid = btns[i].dataset.id
     // console.log(sid)
-    btns[i].addEventListener('click', () => {
-      let currentBtn = document.querySelector('.active-btn')
-      currentBtn.classList.remove('active-btn')
-      btns[i].classList.add('active-btn')
-      let currentsection = document.querySelector('.active')
-      currentsection.classList.remove('active')
-      console.log(currentsection)
-      document.querySelector(`#${btns[i].dataset.id}`).classList.add('active')
+    btns[i].addEventListener("click", () => {
+      let currentBtn = document.querySelector(".active-btn");
+      currentBtn.classList.remove("active-btn");
+      btns[i].classList.add("active-btn");
+      let currentsection = document.querySelector(".active");
+      currentsection.classList.remove("active");
+      console.log(currentsection);
+      document.querySelector(`#${btns[i].dataset.id}`).classList.add("active");
       // console.log(sec)
       // section.classList.add('active')
       // console.log(section)
       // btns[i].dataset.id.classList.add('active')
       // console.log(btns[i].classList)
       // console.log(currentBtn)
-    })
+    });
   }
-
-
 }
 
-pageChange()
+pageChange();
